a78b2d362f9055f02513e1732b0298f577ca6d2c
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2014-10-13 12:44:58 +0530

    Add google maps API and plot location on the map

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var fetchLocation = function (dashElement) {
+var fetchLocation = function (dashElement, mapData) {
+
   var updateStatus = function(alertText) {
     dashElement.innerHTML = alertText;
+  };
+
+  var updateMap = function(latitude, longitude) {
+    mapData.currentLocation = new google.maps.LatLng(latitude, longitude);
+    mapData.map.setCenter(mapData.currentLocation);
+    mapData.marker.setPosition(mapData.currentLocation);
   };
 
   var success = function(location) {
     console.log("Success");
     updateStatus(JSON.stringify(location.coords));
+    updateMap(location.coords.latitude, location.coords.longitude);
   };
 
   var failure = function(err) {
